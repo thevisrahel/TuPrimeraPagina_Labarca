34dbdec2cf0af1eed4ba5bb7fe0f33c96ce950a3
thevisrahel <labarcarelvis@gmail.com>
2026-04-25 21:11:21 -0500

    debug temporal

--- viajes_proyecto/settings.py
+++ viajes_proyecto/settings.py
@@ -20,7 +20,7 @@ load_dotenv()                                                                   
 BASE_DIR = Path(__file__).resolve().parent.parent                                           # Ruta base del proyecto. Todo se calcula a partir de aquí.
 
 
-DEBUG = False                                                                                # True = modo desarrollo (muestra errores detallados). False = modo producción (oculta errores al usuario)
+DEBUG = True                                                                                # True = modo desarrollo (muestra errores detallados). False = modo producción (oculta errores al usuario)
 
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost']      
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mi-app-viajes-4u30.onrender.com']               # Lista de dominios que pueden acceder al proyecto
